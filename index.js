93b9f423cf420e87277681e68703d4012deba65c
Xiomara S <42756313+XioSawin@users.noreply.github.com>
2021-06-02 22:46:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const handlebars = require('express-handlebars');
-//const MongoStore = require('connect-mongo');
 const cluster = require('cluster');
-//const compression = require('compression');
 const log4js = require('log4js');
 const nodemailer = require('nodemailer');
 
 const app = express();
-//app.use(compression());
 
 const User = require('./db/model');
 
@@ -27,10 +24,6 @@ const modoCluster = process.argv[5] == 'CLUSTER';
 /* -------------- PASSPORT w FACEBOOK -------------- */
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
-
-//const bCrypt = require('bCrypt');
-//const LocalStrategy = require('passport-local').Strategy;
-
 
 /* -------------- EMAIL & SMS -------------- */
 
@@ -113,11 +106,6 @@ const enviarSMS = (mensaje) => {
     return rta;   
 }
 
-/*
-const ethereal = require('./email/ethereal.js');
-const gmail = require('./email/gmail.js');
-const twilio = require('./sms/twilio.js'); */
-
 /* -------------- LOGGERS -------------- */
 log4js.configure({
     appenders: {
@@ -184,11 +172,6 @@ if(modoCluster && cluster.isMaster) {
 
     app.use(cookieParser());
     app.use(session({
-        /*
-        store: MongoStore.create({
-            mongoUrl: 'mongodb+srv://XiomaraS:UZosgIq3UUSc8add@coderhouse.j2t64.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-            ttl: 600
-        }),*/
         secret: 'secret',
         resave: false,
         saveUninitialized: false,
@@ -243,13 +226,10 @@ if(modoCluster && cluster.isMaster) {
         let mensaje = `Están intentando iniciar sesión el día ${date}`;
 
         let rta = enviarSMS(mensaje);
-
-        //console.log(rta);
         res.send(rta);
     })
 
     app.get('/welcome', (req, res) => {
-        //console.log(req.user);
 
         res.redirect('/login-email');
     });
@@ -317,23 +297,6 @@ if(modoCluster && cluster.isMaster) {
         res.render("info", info);
     });
 
-    // CHILD PROCESS
-    /*
-    const {fork} = require('child_process');
-
-    // /randoms?cant=20000
-    app.get('/randoms', (req, res) => {
-        try{
-            const randomNumber = fork('./child.js');
-            randomNumber.send(req.query)
-            randomNumber.on('message', numerosRandom => {
-                res.end(`Numeros random ${JSON.stringify(numerosRandom)}`);
-            });
-        } catch (err) {
-            loggerError.error(err);
-        }  
-    }); */
-
     /* -------------- DB CONNECTION -------------- */
 
     app.listen( process.env.PORT|| portCL, ()=>{
@@ -350,133 +313,3 @@ if(modoCluster && cluster.isMaster) {
 
     loggerInfo.info(`Worker ${process.pid} started`);
 };
-
-
-/*
-passport.use('login', new LocalStrategy({
-        passReqToCallback: true
-    },
-    function(req, username, password, done) {
-        // ver en db si existe el username
-        User.findOne({ 'username' : username },
-            function(err, user) {
-                // If there is an error
-                if(err) {
-                    return done(err);
-                }
-                // If username does not exist on db
-                if(!user) {
-                    console.log(`Usuario "${username}" no encontrado`);
-                    console.log('message', 'Usuario no encontrado');
-                    return done(null, false);
-                }
-                // User exists but wrong pwrd
-                if(!isValidPassword(user, password)) {
-                    console.log('Contrasena no valida');
-                    console.log('message', 'Invalid Password');
-                    return done(null, false);
-                }
-                // si alles is goed
-                return done(null, user);
-            }
-        );
-    })
-);
-
-const isValidPassword = function(user, password){
-    return bCrypt.compareSync(password, user.password);
-} */
-
-/* -------------- registration check -------------- */
-/*
-passport.use('register', new LocalStrategy({
-        passReqToCallback: true
-    },
-    function(req, username, password, done) {
-        const findOrCreateUser = function(){
-            User.findOne({'username':username}, function(err, user){
-                if(err){
-                    console.log(`Error en el registro: "${err}"`);
-                    return done(err);
-                }
-                // si user ya existe
-                if (user) {
-                    console.log('Usuario ya existe');
-                    console.log('message', 'Usuario ya existe en la base');
-                    return done(null, false);
-                } else {
-                    // si no existe, crear el usuario
-                    var newUser = new User();
-
-                    newUser.username = username;
-                    newUser.password = createHash(password);
-
-                    newUser.save(function(err) {
-                        if(err) {
-                            console.log(`Error guardando el usuario: "${err}"`);
-                            throw err;
-                        }
-                        console.log('Usuario registrado con exito');
-                        return done(null, newUser);
-                    });
-                }
-            });
-        }
-        process.nextTick(findOrCreateUser);
-    })
-)
-
-*/
-
-/* -------------- crear hash para pwrd -------------- */
-/*/
-const createHash = function(password) {
-    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-}*/
-
-
-
-
-
-
-
-/* -------------- LOGIN Y LOGOUT c/ FB-------------- */
-
-//onst getSessionName = req => req.session.nombre? req.session.nombre: ''
-
-
-
-/*
-app.post('/login', passport.authenticate('login', { failureRedirect: '/faillogin'}), (req, res) => {
-    res.redirect('/')
-}) */
-
-
-
-/*
-app.post('/login', (req, res)=>{
-    let { nombre } = req.body;
-    req.session.nombre = nombre;
-    res.redirect('/');
-}) */
-
-
-
-
-
-/* -------------- REGISTER -------------- */
-
-/*
-app.get('/register', (req, res) => {
-    res.sendFile(process.cwd() + '/public/register.html');
-})
-
-app.post('/register', passport.authenticate('register', {failureRedirect: '/failregister'}), (req, res) => {
-    res.redirect('/');
-})
-
-app.get('/failregister', (req, res) => {
-    res.render('register-error', {});
-})
-*/
-
